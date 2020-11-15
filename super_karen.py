b1e306541014bad8829af3d4c764d3fd403e4c4f
Antoni Marek <antoni.m.marek@gmail.com>
2020-11-15 15:02:48 +0100

    new superheroo

--- super_karen.py
+++ super_karen.py
@@ -3,12 +3,12 @@ class Karen():
     def __init__(self):
         self.name = 'Karen'
         self.superpowers = ['Offend Enemy', 'Talk to manager']
-        self.life_points = random.choice(range(1, 11))
+        self.life_points = random.randint(1, 11)
 
     def attack(self):
-        return random.choice(range(1, 11))
+        return random.randint(1, 11)
 
     def decrease_life(self, x):
-        self.life_points = self.life_points - x
+        self.life_points -= x
 
 super_Karen = Karen()
