137ada566888f44e76d699edb0c65a6e2de27942
magdalena-kuczkowska <74091885+magdalena-kuczkowska@users.noreply.github.com>
2020-11-15 16:07:23 +0100

    Update ghero_class.py

--- ghero_class.py
+++ ghero_class.py
@@ -3,9 +3,9 @@ class Superhero():
     def __init__(self):
         self.name = 'Ice Queen'
         self.superpowers = ['Ice Cannon', 'Blizzard', 'Snow Wind']
-        self.life_points = random.randint(0,10)
+        self.life_points = random.randint(1,10)
     def attack(self):
-        return random.randint(0,10)
+        return random.randint(1,10)
     def decrease_life(self, x):
         self.life_points -= x
 superhero = Superhero()
