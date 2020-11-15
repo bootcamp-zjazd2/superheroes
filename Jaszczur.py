2f46c291088cbbba536d148ffec8b43928eb5503
Szymon Sowiński <szymon.w.sowinski@gmail.com>
2020-11-15 14:44:31 +0100

    to jest moj super bohater

---
+++ Jaszczur.py
@@ -0,0 +1,14 @@
+import random
+
+class Wielki_Jaszczur():
+    def __init__(self,name="LizardMen",superpowers=["plywa", "odrasta mu ogon"]):
+        self.name=name
+        self.superpowers=superpowers
+        self.life_points=random.randint(1,10)
+
+    def attack(self):
+        return random.randint(1,10)
+
+    def decrease_life(self,damage):
+        self.life_points -= damage
+
