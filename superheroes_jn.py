faf30625120fbc5bededf42ee1969120e818536a
Joanna Nowacka <j.nowacka94@gmail.com>
2020-11-15 15:25:24 +0100

    add atributes

--- superheroes_jn.py
+++ superheroes_jn.py
@@ -1,7 +1,15 @@
+import random
+
 class Superheroes():
-    def __init__(self, name, superpowers, life_points):
-        self.name = name
-        self.superpowers = superpowers
-        self.life_points = life_points
-bohater1 = Superheroes('SuperPies', 'lizanie', 100)
-print(bohater1.name, bohater1.superpowers, bohater1.life_points)
+    def __init__(self):
+        self.name = 'SuperPies'
+        self.superpowers = ['lizanie', 'podjadanie']
+        self.life_points = random.randint(1,10)
+
+    def attack(self):
+        return random.randint(1,10)
+
+    def decrease_life(self, x):
+        self.life_points -= x
+
+super_pies = Superheroes()
