33a18e3194068f24cffff2156b12f61be6641499
Joanna Nowacka <j.nowacka94@gmail.com>
2020-11-15 15:37:19 +0100

    changed class

--- superheroes_jn.py
+++ superheroes_jn.py
@@ -1,15 +1,15 @@
 import random
 
-class Superheroes():
+class SuperPies():
     def __init__(self):
-        self.name = 'SuperPies'
-        self.superpowers = ['lizanie', 'podjadanie']
+        self.name = 'Super Lindor'
+        self.superpowers = ['lizanie', 'podjadanie', 'szczekanie']
         self.life_points = random.randint(1,10)
 
     def attack(self):
-        return random.randint(1,10)
+        return random.randint(1, 10)
 
     def decrease_life(self, x):
         self.life_points -= x
 
-super_pies = Superheroes()
+super_pies = SuperPies()
