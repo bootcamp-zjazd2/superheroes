5c94bd68971e85fe68bc2e12ae587add89bf5944
Armand Polanowski <armand1027@gmail.com>
2020-11-15 15:50:58 +0100

    zadanie superbohater

--- moj_plik1.py
+++ moj_plik1.py
@@ -5,10 +5,10 @@ class Kapitan_Marvel():
     def __init__(self):
         self.name = 'Kapitan Marvel'
         self.superpowers = ['Zwiększona siła', 'Wytrzymałość', 'Zwinność', 'Odporność na truciznę']
-        self.life_points = random.randint(1,10)
+        self.life_points = random.randint(1,11)
 
     def attack(self):
-        return random.randint(1,10)
+        return random.randint(1,11)
 
     def decrease_life(self, x):
         self.life_points -= x
