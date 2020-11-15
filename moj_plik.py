3ceffcce149daa44e67b803e2f6704ab78471f13
Piotr Jelonek <piotr.jelonek@gmail.com>
2020-11-15 15:20:37 +0100

    Zmiany zmiany zmiany

---
+++ moj_plik.py
@@ -0,0 +1,13 @@
+import random
+class Heroes:
+    def __init__(self, name, superpowers):
+        self.name = name
+        self.superpowers = superpowers
+        self.life_points = random.randint(1,100)
+
+    def attack(self):
+        random.randint(1,10)
+
+    def decrease_life(self,x):
+        self.life_points -= x
+Squirrelman = Heroes('Squirrelman', ['nut throw', 'scratching'], 100)
